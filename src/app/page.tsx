fcbc6167075d5cc5db87c2269043c32b6121b2f8
bghook <146375034+bghook@users.noreply.github.com>
2024-02-16 14:36:09 -0600

    Add forecast details and icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,8 @@ import axios from "axios";
 import { format, parseISO } from "date-fns";
 import Container from "./components/Container";
 import { convertKelvinToFahrenheit } from "@/utils/convertKelvinToFahrenheit";
+import WeatherIcon from "./components/WeatherIcon";
+import { getDayOrNightIcon } from "@/utils/getDayOrNightIcon";
 
 // type WeatherForecastResponse = {
 //   cod: string;
@@ -188,16 +190,46 @@ export default function Home() {
                       key={i}
                       className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"
                     >
-                      <p>{format(parseISO(d.dt_txt), "h:mm a")}</p>
+                      <p className="whitespace-nowrap">
+                        {format(parseISO(d.dt_txt), "h:mm a")}
+                      </p>
+
+                      {/* <WeatherIcon iconName={d.weather[0].icon} /> */}
+                      <WeatherIcon
+                        iconName={getDayOrNightIcon(
+                          d.weather[0].icon,
+                          d.dt_txt
+                        )}
+                      />
+                      <p>{convertKelvinToFahrenheit(d?.main.temp ?? 0)}°</p>
                     </div>
                   )
                 )}
               </div>
             </Container>
           </div>
+          <div className="flex gap-4">
+            {/* Left container - weather description */}
+            <Container className="w-fit justify-center flex-col px-4 items-center">
+              <p className="capitalize text-center">
+                {firstData?.weather[0].description}
+              </p>
+              <WeatherIcon
+                iconName={getDayOrNightIcon(
+                  firstData?.weather[0].icon ?? "",
+                  firstData?.dt_txt ?? ""
+                )}
+              />
+            </Container>
+            {/* Right container - weather details */}
+            <Container className="bg-blue-400/10 px-6 gap-4 justify-between overflow-x-auto"></Container>
+          </div>
         </section>
+
         {/* 7 day forecast data*/}
-        <section></section>
+        <section className="flex w-full flex-col gap-4">
+          <p className="text-2xl">7-Day Forecast</p>
+        </section>
       </main>
     </div>
   );
